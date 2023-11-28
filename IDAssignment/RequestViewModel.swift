b4481e8011bd315e9fedc1c487afc96625a31b9e
Sanjay Mali <malisanjay5433@gmail.com>
2023-11-28 15:57:47 +0530

    feat : add RequestViewModelProtocol
    feat: add sendRequest() and sendMultipleRequests() handle gracefully in the RequestViewModel

--- IDAssignment/RequestViewModel.swift
+++ IDAssignment/RequestViewModel.swift
@@ -6,26 +6,67 @@
 //
 
 import Foundation
-
-protocol RequestViewModelProtocol {
+import Combine
+protocol RequestViewModelProtocol : ObservableObject{
     func sendRequest()
+    func sendMultipleRequests()
 }
 
 class RequestViewModel: RequestViewModelProtocol {
-    private let serverService: ServerServiceProtocol
+    private let serverService = ServerService()
+    private var cancellables: Set<AnyCancellable> = []
 
-    init(serverService: ServerServiceProtocol) {
-        self.serverService = serverService
-    }
+    @Published var acknowledgmentMessage: String?
+    @Published var chatBotResponse: String?
 
     func sendRequest() {
         serverService.acknowledgeRequest { success in
-            // Handle the acknowledgment, e.g., update UI or perform further actions
-            if success {
-                print("Request Acknowledged!")
-            } else {
-                print("Request Not Acknowledged!")
+            DispatchQueue.main.async {
+                if success {
+                    self.acknowledgmentMessage = "Request Acknowledged!"
+                    print("Response : \(self.acknowledgmentMessage!)")
+                } else {
+                    self.acknowledgmentMessage = "Request Not Acknowledged!"
+                    print("Response : \(self.acknowledgmentMessage!)")
+                }
             }
         }
     }
+
+    func sendChatBotRequest() {
+        serverService.chatBotResponse { response in
+            DispatchQueue.main.async {
+                self.chatBotResponse = response
+            }
+        }
+    }
+
+    func sendMultipleRequests() {
+        let acknowledgmentPublisher = Future<Bool, Never> { promise in
+            self.serverService.acknowledgeRequest { success in
+                promise(.success(success))
+            }
+        }
+
+        let chatBotResponsePublisher = Future<String, Never> { promise in
+            self.serverService.chatBotResponse { response in
+                promise(.success(response))
+            }
+        }
+
+        Publishers.CombineLatest(acknowledgmentPublisher, chatBotResponsePublisher)
+            .sink { ackSuccess, chatResponse in
+                DispatchQueue.main.async {
+                    if ackSuccess {
+                        self.acknowledgmentMessage = "Requests Acknowledged!"
+                    } else {
+                        self.acknowledgmentMessage = "Requests Not Acknowledged!"
+                    }
+                    print("ackSuccess: \(ackSuccess), chatResponse : \(chatResponse)")
+                    self.chatBotResponse = chatResponse
+                }
+            }
+            .store(in: &cancellables)
+    }
 }
+
