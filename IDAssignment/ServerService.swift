1fe631d1a73928b6b2ab7a8e9036f874165691bd
Sanjay Mali <malisanjay5433@gmail.com>
2023-11-28 15:53:31 +0530

    feat: add chatBotResponse() implementation

--- IDAssignment/ServerService.swift
+++ IDAssignment/ServerService.swift
@@ -8,6 +8,7 @@
 import Foundation
 protocol ServerServiceProtocol {
     func acknowledgeRequest(completion: @escaping (Bool) -> Void)
+    func chatBotResponse(completion: @escaping (String) -> Void)
 }
 class ServerService: ServerServiceProtocol {
     func acknowledgeRequest(completion: @escaping (Bool) -> Void) {
@@ -15,6 +16,15 @@ class ServerService: ServerServiceProtocol {
         DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
             // Simulate server acknowledging the request
             completion(true)
+        }
+    }
+    func chatBotResponse(completion: @escaping (String) -> Void) {
+        // Simulate chat bot response delay
+        DispatchQueue.global().asyncAfter(deadline: .now() + Double.random(in: 1...5)) {
+            // Simulate chat bot response
+            let responses = ["How can I assist you?", "Interesting question!", "I'm here to help!"]
+            let randomResponse = responses.randomElement() ?? "I didn't understand that."
+            completion(randomResponse)
         }
     }
 }
