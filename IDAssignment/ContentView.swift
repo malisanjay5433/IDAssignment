93db38efaf3b1296646bbc2f23d95698a22321cc
Sanjay Mali <malisanjay5433@gmail.com>
2023-11-28 15:59:57 +0530

    feat: add UI components to intereact with the requests

--- IDAssignment/ContentView.swift
+++ IDAssignment/ContentView.swift
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
+    @ObservedObject private var requestViewModel = RequestViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("IDNOW CHATBOT")
+                .foregroundColor(.orange)
+                .bold()
+                .padding()
         }
-        .padding()
+        HStack {
+            Button("Send Request") {
+                requestViewModel.sendRequest()
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+            Button("Send Multiple Requests") {
+                requestViewModel.sendMultipleRequests()
+            }
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+          }
+        VStack{
+            Button("Chat with Bot") {
+                requestViewModel.sendChatBotRequest()
+            }
+            .padding()
+            .buttonStyle(.bordered)
+            
+            if let acknowledgmentMessage = requestViewModel.acknowledgmentMessage {
+                Text(acknowledgmentMessage)
+                    .foregroundColor(acknowledgmentMessage.contains("Not") ? .red : .green)
+                    .fontWeight(.heavy)
+                    .padding()
+
+            }
+            if let chatBotResponse = requestViewModel.chatBotResponse {
+                Text("Heyy! I'm bot: \(chatBotResponse)")
+                    .foregroundColor(.primary)
+                    .italic()
+                    .padding()
+            }
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
