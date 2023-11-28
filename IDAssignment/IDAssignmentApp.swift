6024f28ade82ebccb36ff0b399c5c01b9db8c5da
Sanjay Mali <malisanjay5433@gmail.com>
2023-11-28 14:47:26 +0530

    setup: initial commit

---
+++ IDAssignment/IDAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDAssignmentApp.swift
+//  IDAssignment
+//
+//  Created by Sanjay Mali on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IDAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
